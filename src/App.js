5b8005c94d792c29e33eb272b35785f354fb9be1
estelletchamani5 <estelletchamani5@gmail.com>
2022-01-11 03:37:11 +0100

    finally

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <h1>MY first app with react , using travis ci and github page for deployement</h1>
         </p>
         <a
           className="App-link"
