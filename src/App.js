a0e433bb3d1512a04ba4cb18ed6d4063735c9481
estelletchamani5 <estelletchamani5@gmail.com>
2022-01-12 15:55:15 +0100

    remove link

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
          <h1>MY first app with react , using travis ci and github page for deployement</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
